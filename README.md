460303f75f1b6609eb97001110374ac7346269eb
Yuri Rodrigues <yurihenriques2004@gmail.com>
2025-12-21 08:16:25 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Banking System
-A simple banking system is coming to be creat 
+A banking system is coming to be creat 
--- system.py
+++ system.py
@@ -15,23 +15,51 @@ class customer:
 
 
 class Account:
-    def __init__(self, number_account, customer, balance):
+    def __init__(self, number_account, customer):
         self.customer = customer
-        self.balance = balance
+        self.balance = 0.0
         self.number_account = number_account
 
     def deposit(self, value):
         if value <= 0:
             print(f"You cannot deposit negative or zero values")
+            return False
         else:
             self.balance += value
+            return True
 
     def withdraw(self, value):
         if self.balance >= value:
             self.balance -= value
+            return True
         else:
             print(f"Not enough money")
+            return False
 
     def print_balance(self):
         print(f'Name:{self.customer.name}')
         print(f"Balance:{self.balance}")
+
+
+class Bank:
+    list_of_Accounts = []
+
+    def __init__(self, name):
+        self.name = name
+
+    def creat_account(self, customer):
+        number_account = random.randint(10000000000000, 99999999999999)
+        account = Account(number_account, customer)
+        Bank.list_of_Accounts.append(account)
+
+    def search_account(self, number_account):
+        for account in Bank.list_of_Accounts:
+            if account.number_account == number_account:
+                return account
+        return None
+
+    def transfer_account(self, account1, account2, amount):
+        if self.search_account(account1) and self.search_account(account2) is not None:
+            account1.withdraw(amount)
+            account2.deposit(amount)
+        
