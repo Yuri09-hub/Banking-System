0285bc7518efc02ec74dab7152e8c934fa747f73
Yuri Rodrigues <yurihenriques2004@gmail.com>
2025-12-21 07:02:15 +0100

    upadate

--- system.py
+++ system.py
@@ -1,9 +1,12 @@
+import random
+
+
 class customer:
     def __init__(self, name, address, document, type):
         self.name = name
         self.address = address
-        self.document = document
         self.type = type
+        self.document = document
 
     def print_customer(self):
         print(f"Name: {self.name}")
@@ -11,19 +14,15 @@ class customer:
         print(f"{self.type}_ID:{self.document}")
 
 
-class Account(customer):
-    def __init__(self, Number_account, balance, name, address, document, type):
-        super().__init__(name, address, document, type)
-        self.Number_account = Number_account
+class Account:
+    def __init__(self, number_account, customer, balance):
+        self.customer = customer
         self.balance = balance
-        self.name = name
-        self.address = address
-        self.document = document
-        self.type = type
+        self.number_account = number_account
 
     def deposit(self, value):
         if value <= 0:
-            print(f"You cannot deposit negative values")
+            print(f"You cannot deposit negative or zero values")
         else:
             self.balance += value
 
@@ -34,5 +33,5 @@ class Account(customer):
             print(f"Not enough money")
 
     def print_balance(self):
-        print(f"Name: {self.name}")
+        print(f'Name:{self.customer.name}')
         print(f"Balance:{self.balance}")
